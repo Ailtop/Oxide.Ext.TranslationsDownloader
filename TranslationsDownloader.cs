7843d19b5667f95915214f5c8df453228394877d
Ailtop <443020352@qq.com>
2021-02-23 18:09:45 +0800

    Initial commit

---
+++ TranslationsDownloader.cs
@@ -0,0 +1,27 @@
+﻿using Oxide.Core;
+using Oxide.Core.Plugins;
+
+namespace TranslationsDownloader
+{
+    public class TranslationsDownloader : CSPlugin
+    {
+        public TranslationsDownloader()
+        {
+            Title = "TranslationsDownloader";
+            Author = "Arainrr";
+            Version = new VersionNumber(1, 0, 0);
+            Description = "Download TranslationsDownloader";
+        }
+
+        [HookMethod("OnTerrainInitialized")]
+        private void OnTerrainInitialized()
+        {
+            Interface.Oxide.LogError("OnTerrainInitialized");
+            //foreach (var singletonComponent in UnityEngine.Object.FindObjectsOfType<SingletonComponent>())
+            //{
+            //    Interface.Oxide.LogError($"{singletonComponent?.name} : {singletonComponent?.GetType()}");
+            //}
+            TranslationsExtension.Instance.Initialize();
+        }
+    }
+}
---
+++ TranslationsLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Oxide.Core.Plugins;
+
+namespace TranslationsDownloader
+{
+    internal class TranslationsLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[]
+        {
+            typeof(TranslationsDownloader)
+        };
+    }
+}
