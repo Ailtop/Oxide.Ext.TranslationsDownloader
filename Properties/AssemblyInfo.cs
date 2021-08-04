1c970f23aa22b0625fcd61660c274f9f18ba9746
liujunming <liujunming@aobi.com>
2021-08-04 14:38:13 +0800

    Change path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Translations Downloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Download Translation Files for Rust")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arainrr")]
 [assembly: AssemblyProduct("TranslationsDownloader")]
